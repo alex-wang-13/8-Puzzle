408f7d2f83bb0fbbe1e267c812f4977f4e8ae456
Alex Wang <iamalex11303@gmail.com>
2023-09-08 18:14:07 -0400

    Created Puzzle struct to hold the state of the 8-puzzle. Wrote skeleton code for the file command parsing.

---
+++ puzzle.py
@@ -0,0 +1,71 @@
+class Puzzle:
+    """
+    A simple struct for holding the puzzle state.
+
+    Class Attributes:
+        state (list[int]):
+            A list representing the current state of the 8-puzzle.
+
+        valid (bool):
+            A boolean value that records the validity of the current puzzle
+            state. This entails that all values in the range [0, 8] are present
+            in the puzzle and the length of the state is exactly 9 elements.
+    """
+    
+    state: list[int] = []
+    valid: bool = False
+
+    def set_state(state: str) -> None:
+        """
+        A function to set and validate the state of the puzzle with a new state.
+
+        Parameters: 
+            state (str):
+                A string representing a state in the form "xxx xxx xxx" where each
+                value x is a number from [0, 9).
+
+        Raises:
+            ValueError:
+                When the given input state does not meet the requirements specified
+                in the parameter desceription.
+        """
+
+        # Trim all spaces from the input.
+        state: list[int] = [int(number) for number in state.replace(" ", "")]
+
+        # Validate the values of the proposed input state.
+        if len(state) != 9:
+            raise ValueError("A state with an invalid length was passed to the puzzle.")
+        for n in range(9):
+            if n not in state:
+                raise ValueError(f"A state is missing the number ${n} in the range [0, 8].")
+        
+        # Update the puzzle state.
+        Puzzle.state = state
+        Puzzle.valid = True
+
+    def reset_puzzle() -> None:
+        """
+        A method to reset the state of the puzzle to its default/intitial state.
+        """
+
+        Puzzle.state: list[int] = []
+        Puzzle.valid: bool = False
+
+# For command line parsing.
+import sys
+
+if __name__ == "__main__":
+    """
+    Reads commands from the file specified in the command line argument.
+    """
+
+    # Check for text file argument.
+    if len(sys.argv) < 2:
+        print("Requires 1 valid file name.")
+
+    # Read commands from the given text file.
+    with open(sys.argv[1]) as file:
+        for line in file:
+            # Do each action in the file.
+            action(line)

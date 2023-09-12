83b8900749f0d2a66a0e5f46567a1f1c4cc89d72
Alex Wang <iamalex11303@gmail.com>
2023-09-12 01:36:54 -0400

    Added the move function to perform a move on tiles in the puzzle and the _swap helper function to swap two tiles in the puzzle.

--- puzzle.py
+++ puzzle.py
@@ -46,7 +46,7 @@ class Puzzle:
         Raises:
             ValueError:
                 When the given input state does not meet the requirements specified
-                in the parameter desceription.
+                in the parameter description.
         """
 
         # Trim all spaces from the input.
@@ -70,6 +70,67 @@ class Puzzle:
             print(Puzzle.state[0], Puzzle.state[1], Puzzle.state[2])
             print(Puzzle.state[3], Puzzle.state[4], Puzzle.state[5])
             print(Puzzle.state[6], Puzzle.state[7], Puzzle.state[8])
+
+    def _swap(p1: int, p2: int) -> None:
+        """
+        A function to swap two tiles in the puzzle state.
+
+        Parameters:
+            p1 (int):
+                The first position to swap.
+
+            p2 (int):
+                The second position to swap.
+
+        Raises: 
+            IndexError:
+                When either position is not in the range [0, 8].
+        """
+
+        if Puzzle.valid:
+            Puzzle.state[p1], Puzzle.state[p2] = Puzzle.state[p2], Puzzle.state[p1]
+
+    def move(move: str) -> None:
+        """
+        A function to move the blank tile in the puzzle.
+
+        Parameters:
+            move (str):
+                A string representing the desired direction of movement for the
+                blank tile.
+
+        Raises:
+            RuntimeError:
+                When the direction of movement is not possible for the blank tile.
+            
+            ValueError:
+                When the given move direction is not understandable (i.e. not "up",
+                "down", "left", or "right").
+        """
+
+        if Puzzle.valid:
+            move: str = move.strip().lower()
+            # Get the position of the blank tile.
+            b_pos: int = Puzzle.state.index(0)
+            match move:
+                case "up":
+                    if b_pos < 4:
+                        raise RuntimeError("It is not possible to move the blank tile up.")
+                    Puzzle._swap(b_pos, b_pos-3)
+                case "down":
+                    if b_pos > 5:
+                        raise RuntimeError("It is not possible to move the blank tile down.")
+                    Puzzle._swap(b_pos, b_pos+3)
+                case "left":
+                    if b_pos % 3 == 0:
+                        raise RuntimeError("It is not possible to move the blank tile left.")
+                    Puzzle._swap(b_pos, b_pos-1)
+                case "right":
+                    if b_pos % 3 == 2:
+                        raise RuntimeError("It is not possible to move the blank tile right.")
+                    Puzzle._swap(b_pos, b_pos+1)
+                case _:
+                    raise ValueError('The move direction is not "up", "down", "left", or "right".')
 
     def reset_puzzle() -> None:
         """
